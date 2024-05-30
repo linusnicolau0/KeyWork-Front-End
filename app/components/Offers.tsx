19971271b6a6468838aac43fc436103474b15c96
pdpau <56670577+pdpau@users.noreply.github.com>
2024-05-30 18:41:59 +0200

    todos

--- app/components/Offers.tsx
+++ app/components/Offers.tsx
@@ -13,11 +13,11 @@ export function Offers ({ offers }: Props) {
                                                 "grid lg:grid-cols-3 sm:grid-cols-2"
                                             )}>
             {offers.map(offer => (
-                <Card className="w-80 rounded-lg overflow-hidden shadow-lg">
+                <Card className="w-80 rounded-lg overflow-hidden shadow-lg"> // TODO: Canviar color border
                     <img src={offer.imageUrl} alt={offer.title} className="w-full h-40 object-cover" />
-                    <div className="p-4 bg-black text-white flex items-center">
+                    <div className="p-3 bg-black text-white flex items-center">
                         <div>
-                            <h2 className="text-lg font-semibold">{offer.title}</h2>
+                            <h2 className="text-lg font-semibold">{offer.title}</h2> // TODO: Posar 3 punts abans que salti de linea
                             <p className="text-sm text-gray-400">{offer.date}</p>
                         </div>
                         <div className="ml-auto">
