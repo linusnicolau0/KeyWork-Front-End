d46e11a96f6d6b2e80b3f9f09e35aafaeeb66a1a
Linus Nicolau <131747202+linusnicolau0@users.noreply.github.com>
2024-06-04 20:32:36 +0200

    axios

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,27 @@
+"use client"
+
 import Image from "next/image"
 import Link from "next/link"
 {/*Desktop logo se usara para navegacion con PC*/}
 import KeyworkLogo from '../icons/keywork_icon.png'
 import { UserNav } from "./UserNav";
 import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+import axios from "../api/axios.js";
 
 export function Navbar() 
 {
+    let userAuth;
+
+    useEffect(() => {
+        axios.get('/api/user').then((response: any) => {
+            userAuth = response.data;
+            console.log(userAuth);
+        });
+
+    }, []);
+
+
     return (
         <nav className="bg-gray h-[100px] flex justify-center items-center px-6 py-4">
 
@@ -30,7 +45,6 @@ export function Navbar()
 
                 {/*La distribució dels botons es farà un cop estigui feta la base de dades i el sistema de login funcioni*/}
 
-                {/*Boton de crear oferta*/}
                 <Button className="bg-white mr-10 mt-1.5 hover:bg-[#b3b4b6] rounded-xl" asChild>
                     {/*Canviar href per redirigir a la pagina de creació d'ofertes (pendent de creació)*/}
                     <Link href="/">
@@ -38,22 +52,31 @@ export function Navbar()
                     </Link>
                 </Button>
 
-                {/*Boton de inicio de sesion*/}
-                <Button className="bg-white mr-10 mt-1.5 hover:bg-[#b3b4b6] rounded-xl">
-                    <Link href="/login">
-                        <h1 className="text-black">Iniciar Sesion</h1>
-                    </Link>
-                </Button>
+                {userAuth === null ? (
+                    <>
+                        <div>
+                            <h1 className="text-white">hola</h1>
+                        </div>
 
-                {/*Boton de registro*/}
-                <Button className="bg-white mr-6 mt-1.5 hover:bg-[#b3b4b6] rounded-xl">
-                    <Link href="/register">
-                        <h1 className="text-black">Registrarse</h1>
-                    </Link>
-                </Button>
+                        {/*Foto de usuario*/}
+                        <UserNav />
 
-                {/*Foto de usuario*/}
-                <UserNav />
+                    </>
+                ) : (
+                    <>
+                        <Button className="bg-white mr-10 mt-1.5 hover:bg-[#b3b4b6] rounded-xl">
+                            <Link href="/login">
+                                <h1 className="text-black">Iniciar Sesion</h1>
+                            </Link>
+                        </Button>
+
+                        <Button className="bg-white mr-6 mt-1.5 hover:bg-[#b3b4b6] rounded-xl">
+                            <Link href="/register">
+                                <h1 className="text-black">Registrarse</h1>
+                            </Link>
+                        </Button>
+                    </>
+                )}
 
             </div>
         </nav>
